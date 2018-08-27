59c545aa28760d085da0eb2b526b1a50a7c19c60
Mike DeLong <mikedelong@outlook.com>
2018-08-27 12:46:38 -0400

    feature: double loop
    Now we are looping over the test size and we are running 500 random seeds per.

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ print('data has %d rows and %d columns' % data.shape)
 model = DecisionTreeRegressor()
 
 test_size = 0.10
-scores = list()
-for random_state in range(1000):
-  X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=test_size, random_state=random_state)
-  model.fit(X_train, y_train)
-  score = model.score(X_test, y_test)
-  scores.append(score)
-print(sum(scores)/len(scores))
+for test_size in [0.7, 0.8, 0.9]:
+  scores = list()
+  for random_state in range(500):
+    X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=test_size, random_state=random_state)
+    model.fit(X_train, y_train)
+    score = model.score(X_test, y_test)
+    scores.append(score)
+  print('with test size %.2f we have score %.3f' % (test_size, sum(scores)/len(scores)))
 
 print('done')

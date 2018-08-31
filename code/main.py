be872eef47b30af18dd8ffb776c6b42fdab7ca4b
mikedelong <mikedelong@outlook.com>
2018-08-31 08:09:15 -0400

    refactor: get the root object rather than the data and target separately

--- code/main.py
+++ code/main.py
@@ -4,8 +4,8 @@ from sklearn.tree import DecisionTreeRegressor
 
 print('started')
 return_X_y = True
-(data, target) = load_diabetes(return_X_y=return_X_y)
-print('data has %d rows and %d columns' % data.shape)
+diabetes = load_diabetes(return_X_y=return_X_y)
+print('data has %d rows and %d columns' % diabetes.data.shape)
 
 model = DecisionTreeRegressor()
 
@@ -13,8 +13,9 @@ test_size = 0.10
 for test_size in [0.7, 0.8, 0.9]:
     scores = list()
     for random_state in range(500):
-        X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=test_size,
-                                                            random_state=random_state)
+        X_train, X_test, y_train, y_test = \
+            train_test_split(diabetes.data, diabetes.target, test_size=test_size,
+                             random_state=random_state)
         model.fit(X_train, y_train)
         score = model.score(X_test, y_test)
         scores.append(score)

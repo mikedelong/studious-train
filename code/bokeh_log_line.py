fd8e67768ebb2c435af7c4a053c5e1b74652b797
mikedelong <mikedelong@outlook.com>
2018-10-24 10:15:36 -0400

    cleanup: single not double quotes

--- code/bokeh_log_line.py
+++ code/bokeh_log_line.py
@@ -8,22 +8,22 @@ y1 = [10 ** i for i in x]
 y2 = [10 ** (i ** 2) for i in x]
 
 # output to static HTML file
-output_file("log_lines.html")
+output_file('log_lines.html')
 
 # create a new plot
 p = figure(
-    tools="pan,box_zoom,reset,save",
-    y_axis_type="log", y_range=[0.001, 10 ** 11], title="log axis example",
+    tools='pan,box_zoom,reset,save',
+    y_axis_type='log', y_range=[0.001, 10 ** 11], title='log axis example',
     x_axis_label='sections', y_axis_label='particles'
 )
 
 # add some renderers
-p.line(x, x, legend="y=x")
-p.circle(x, x, legend="y=x", fill_color="white", size=8)
-p.line(x, y0, legend="y=x^2", line_width=3)
-p.line(x, y1, legend="y=10^x", line_color="red")
-p.circle(x, y1, legend="y=10^x", fill_color="red", line_color="red", size=6)
-p.line(x, y2, legend="y=10^x^2", line_color="orange", line_dash="4 4")
+p.line(x, x, legend='y=x')
+p.circle(x, x, legend='y=x', fill_color='white', size=8)
+p.line(x, y0, legend='y=x^2', line_width=3)
+p.line(x, y1, legend='y=10^x', line_color='red')
+p.circle(x, y1, legend='y=10^x', fill_color='red', line_color='red', size=6)
+p.line(x, y2, legend='y=10^x^2', line_color='orange', line_dash='4 4')
 
 # show the results
 show(p)

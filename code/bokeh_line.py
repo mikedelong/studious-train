fb88e9d2a7784daeaac589cdf3a1098c03ddf12a
mikedelong <mikedelong@outlook.com>
2018-10-24 10:15:11 -0400

    cleanup: single not double quotes

--- code/bokeh_line.py
+++ code/bokeh_line.py
@@ -6,13 +6,13 @@ x = [1, 2, 3, 4, 5]
 y = [6, 7, 2, 4, 5]
 
 # output to static HTML file
-output_file("lines.html", title="line plot example")
+output_file('lines.html', title='line plot example')
 
 # create a new plot with a title and axis labels
-p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+p = figure(title='simple line example', x_axis_label='x', y_axis_label='y')
 
 # add a line renderer with legend and line thickness
-q = p.line(x, y, legend="Temp.", line_width=2)
+q = p.line(x, y, legend='Temp.', line_width=2)
 
 # show the results
 show(p)

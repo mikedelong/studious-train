dfebaa3b5dbdce174954283df3732a9b52b75167
mikedelong <mikedelong@outlook.com>
2018-11-04 15:29:31 -0500

    feature: calcium uptake data from R datasets

--- code/get_datasets.py
+++ code/get_datasets.py
@@ -249,6 +249,20 @@ if __name__ == '__main__':
     breslow_title = breslow_bundle.title
     logger.info('breslow data has title %s' % breslow_title)
 
+    logger.info('loading calcium uptake data')
+    calcium_pickle = data_folder + 'calcium.pkl'
+    if exists(calcium_pickle):
+        with open(calcium_pickle, 'rb') as calcium_fp:
+            calcium_bundle = pickle.load(calcium_fp)
+    else:
+        calcium_bundle = get_rdataset('calcium', 'boot')
+        with open(calcium_pickle, 'wb') as calcium_fp:
+            pickle.dump(calcium_bundle, calcium_fp)
+    calcium_data = calcium_bundle.data
+    logger.info('calcium data has variables %s' % list(calcium_data))
+    calcium_title = calcium_bundle.title
+    logger.info('calcium data has title %s' % calcium_title)
+
     logger.info('loading cancer data')
     cancer_pickle = data_folder + 'cancer.pkl'
     if exists(cancer_pickle):

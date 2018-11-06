534c1b71108943ae4ff69a75972d0eaa738dc1fd
mikedelong <mikedelong@outlook.com>
2018-11-06 12:49:06 -0500

    new file: basic Dash app with CSS loaded from the local file system

---
+++ plotting/dash_basic.py
@@ -0,0 +1,80 @@
+# https://community.plot.ly/t/multiple-plots-running-on-frames/8235/8
+
+import logging
+from datetime import datetime
+from time import time
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import numpy as np
+import pandas as pd
+
+if __name__ == '__main__':
+    start_time = time()
+
+    console_formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+    file_formatter = logging.Formatter('%(asctime)s : %(message)s')
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(console_formatter)
+    console_handler.setLevel(logging.INFO)
+    logger.addHandler(console_handler)
+
+    logger.info('started')
+
+    # first create our bogus source data
+    periods = 2000
+    slice_count = 10
+    slice_size = periods // slice_count
+    sqrt_periods = int(np.sqrt(float(periods)))
+    start = datetime(2018, 4, 15, 0, 0, 0)
+    dates = pd.date_range(start=start, periods=periods, freq='S')
+    x = np.linspace(start=0, stop=periods + 1, num=periods).transpose()
+    y = np.linspace(start=0, stop=sqrt_periods, num=periods).transpose()
+    y = y * y
+    z = np.linspace(start=0, stop=periods + 1, num=periods).transpose()
+    speed = np.linspace(start=0, stop=periods + 1, num=periods).transpose()
+    phenomenon = [1.0 + 0.05 * np.random.uniform(0, 1) for j in range(periods)]
+    df = pd.DataFrame.from_dict(
+        {'dates': dates, 'x': x, 'y': y, 'z': z, 'speed': speed, 'phenomenon': phenomenon}).set_index('dates')
+    df['color'] = (256.0 * df['speed'] / float(periods)).astype('int32')
+
+    # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+    dcc._css_dist[0]['relative_package_path'].append('bWLwgP.css')
+
+    app = dash.Dash(__name__)
+
+    app.layout = html.Div(children=[
+        html.H1(children='Hello Dash'),
+
+        html.Div(children='''
+            Dash: A web application framework for Python.
+        '''),
+
+        dcc.Graph(
+            id='example-graph',
+            figure={
+                'data': [
+                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                ],
+                'layout': {
+                    'title': 'Dash Data Visualization'
+                }
+            }
+        )
+    ])
+
+    app.run_server(debug=True)
+
+    logger.info('done')
+
+    finish_time = time()
+    elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+    elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+    logger.info('Time: {:0>2}:{:0>2}:{:05.2f}'.format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
+    console_handler.close()
+    logger.removeHandler(console_handler)

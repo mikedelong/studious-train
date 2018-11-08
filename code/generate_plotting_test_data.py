91ebcded1e1496c3a066139c9a47abcad9453b5e
mikedelong <mikedelong@outlook.com>
2018-11-08 07:48:32 -0500

    feature: logging and total time; initial copy-paste from source

---
+++ code/generate_plotting_test_data.py
@@ -0,0 +1,45 @@
+import logging
+from datetime import datetime
+from time import time
+
+import numpy as np
+import pandas as pd
+
+if __name__ == '__main__':
+    start_time = time()
+
+    console_formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+    file_formatter = logging.Formatter('%(asctime)s : %(message)s')
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(console_formatter)
+    console_handler.setLevel(logging.INFO)
+    logger.addHandler(console_handler)
+
+    logger.info('started')
+
+    # first create our bogus source data
+    periods = 2000
+    sqrt_periods = int(np.sqrt(float(periods)))
+    start = datetime(2018, 4, 15, 0, 0, 0)
+    dates = pd.date_range(start=start, periods=periods, freq='S')
+    x = np.linspace(start=0, stop=periods + 1, num=periods).transpose()
+    y = np.linspace(start=0, stop=sqrt_periods, num=periods).transpose()
+    y = y * y
+    z = np.linspace(start=0, stop=periods + 1, num=periods).transpose()
+    speed = np.linspace(start=0, stop=periods + 1, num=periods).transpose()
+    noise = [1.0 + 0.05 * np.random.uniform(0, 1) for j in range(periods)]
+    df = pd.DataFrame.from_dict({'dates': dates, 'x': x, 'y': y, 'z': z, 'speed': speed, 'noise': noise}).set_index(
+        'dates')
+    df['color'] = (256.0 * df['speed'] / float(periods)).astype('int32')
+
+    logger.info('done')
+
+    finish_time = time()
+    elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+    elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+    logger.info('Time: {:0>2}:{:0>2}:{:05.2f}'.format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
+    console_handler.close()
+    logger.removeHandler(console_handler)

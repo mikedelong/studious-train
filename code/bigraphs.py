665ebda051ce6920ae9876b2f73459e0aab5790c
mikedelong <mikedelong@outlook.com>
2018-12-05 08:02:30 -0500

    feature: we have renamed this file and reduced it to a shell to start our bigraphs work.

---
+++ code/bigraphs.py
@@ -0,0 +1,26 @@
+import logging
+from time import time
+
+if __name__ == '__main__':
+    start_time = time()
+
+    console_formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+    file_formatter = logging.Formatter('%(asctime)s : %(message)s')
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(console_formatter)
+    console_handler.setLevel(logging.INFO)
+    logger.addHandler(console_handler)
+
+    logger.info('started')
+
+    logger.info('done')
+
+    finish_time = time()
+    elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+    elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+    logger.info('Time: {:0>2}:{:0>2}:{:05.2f}'.format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
+    console_handler.close()
+    logger.removeHandler(console_handler)
--- code/import_tests.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-t0 = np.array([1, 1])
-print(t0)
-
-t1 = np.nan
-print(t1)

6a03557fdb0ad5fe9c8d7b73a71d245ece66d6ee
mikedelong <mikedelong@outlook.com>
2018-12-05 10:09:32 -0500

    feature: first working version to use the book/author data
    Signed-off-by: mikedelong <mikedelong@outlook.com>

---
+++ code/bigrams.py
@@ -0,0 +1,74 @@
+import logging
+from time import time
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+
+def modify_author(arg):
+    if ',' not in arg:
+        return arg
+    else:
+        (piece0, piece1) = arg.split(',')
+        return ' '.join([piece1, piece0]).strip()
+
+
+if __name__ == '__main__':
+    start_time = time()
+
+    console_formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(console_formatter)
+    console_handler.setLevel(logging.INFO)
+    logger.addHandler(console_handler)
+
+    logger.info('started')
+
+    usecols = ['Title', 'Author']
+    source_df = pd.read_csv('../data/books.csv', usecols=usecols)
+    logger.info(list(source_df))
+    logger.info(source_df.head())
+
+    source_df = source_df.dropna()
+
+    source_df['modified_author'] = source_df['Author'].apply(modify_author)
+    names = source_df['modified_author'].dropna().values
+    bogeys = source_df['Title'].dropna().values
+    # build the bigrams list; this is our feature list
+    bigrams = sorted(
+        list(set([item[i:i + 2] for item in names.tolist() + bogeys.tolist() for i in range(len(item) - 1)])))
+    logger.info('we found %d bigrams' % len(bigrams))
+    data_dict = dict()
+    for item in set(names.tolist() + bogeys.tolist()):
+        logger.info('%s %s' % (item, item in names))
+        data_dict[item] = [int(bigram in item) for bigram in bigrams] + [int(item in names)]
+
+    data_df = pd.DataFrame.from_dict(data_dict, orient='index', columns=bigrams + ['target'])
+    logger.info('our data has %d rows and %d columns' % data_df.shape)
+
+    test_size = 0.33
+    split_random_state = 1
+    X_train, X_test, y_train, y_test = train_test_split(data_df[bigrams], data_df['target'].values,
+                                                        test_size=test_size, random_state=split_random_state)
+
+    for index, random_state in enumerate(range(1000)):
+        model = DecisionTreeClassifier(random_state=random_state)
+        model.fit(X=X_train, y=y_train)
+        y_predicted = model.predict(X=X_test)
+        current = y_predicted if index == 0 else (float(index) * current + y_predicted) * (1.0 / float(index + 1))
+        if index % 10 == 0:
+            logger.info('%d names: %s \nexpected: %s \nactual: %s \ncurrent: %s' %
+                        (random_state, X_test.index.values, y_test, y_predicted, current))
+
+    logger.info('done')
+
+    finish_time = time()
+    elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+    elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+    logger.info('Time: {:0>2}:{:0>2}:{:05.2f}'.format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
+    console_handler.close()
+    logger.removeHandler(console_handler)

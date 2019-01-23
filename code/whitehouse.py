8707b23877c8ff8b79ad3673b3b35ca16c35bc4b
mikedelong <mikedelong@outlook.com>
2019-01-23 12:13:56 -0500

    feature: working dedupe example; not especially satisfactory at the moment due to the amount of manual work required
    Signed-off-by: mikedelong <mikedelong@outlook.com>

--- code/whitehouse.py
+++ code/whitehouse.py
@@ -3,6 +3,7 @@ import logging
 from os.path import exists
 from time import time
 
+import dedupe
 import pandas as pd
 
 if __name__ == '__main__':
@@ -32,9 +33,41 @@ if __name__ == '__main__':
         input_df = pd.read_csv(url)
         input_df.to_csv(input_file, index=False)
 
+    nrows = 1000
     logger.info('loading data from {}'.format(input_file))
-    input_df = pd.read_csv(input_file)
+    input_df = pd.read_csv(input_file, nrows=nrows)
+    # clean up the column names
+    input_df.columns = [item.strip() for item in input_df.columns]
+    logger.info(list(input_df))
+    # input_df['date'] = input_df['date'].astype('datetime64[ms]')
+    for column in list(input_df):  # [item for item in list(input_df) if item != 'date']:
+        logger.info(column)
+        input_df[column] = input_df[column].astype(str)
+        input_df[column] = input_df[column].apply(lambda x: x.strip())
+
+    # it turns out the gender column is no good
+    input_df = input_df.drop(['gender'], axis=1)
+    logger.info(input_df['race'].unique())
+
     logger.info('our raw input data has {} rows and {} columns'.format(len(input_df), len(list(input_df))))
+    logger.info('our data has columns {}'.format(list(input_df)))
+    logger.info('if we remove duplicates we have only {} rows'.format(len(input_df.drop_duplicates())))
+
+    input_dict = {index: value for index, value in enumerate(input_df.to_dict(orient='records'))}
+
+    training_file = './whitehouse_dedupe_training.json'
+    fields = [
+        {'field': 'visitor_name', 'type': 'String'},
+        {'field': 'race', 'type': 'String', 'has missing': True},
+    ]
+    deduper = dedupe.Dedupe(fields)
+    deduper.sample(input_dict, 30)  # let's start with 20 because apparently 100 is too many
+    dedupe.consoleLabel(deduper)
+    deduper.train()
+    logger.info('writing training results to {}'.format(training_file))
+    with open(training_file, 'w') as training_fp:
+        deduper.writeTraining(training_fp)
+
     logger.info('done')
 
     finish_time = time()

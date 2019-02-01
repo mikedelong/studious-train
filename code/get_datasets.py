8f86f74eef20ff35b9bdcc7a1c21245a241aba79
mikedelong <mikedelong@outlook.com>
2019-02-01 08:13:47 -0500

    feature: added plutonium batch data from R datasets
    Signed-off-by: mikedelong <mikedelong@outlook.com>

--- code/get_datasets.py
+++ code/get_datasets.py
@@ -1670,6 +1670,34 @@ if __name__ == '__main__':
     plantTraits_title = plantTraits_bundle.title
     logger.info('plantTraits data has title %s' % plantTraits_title)
 
+    logger.info('loading plant species traits data')
+    plantTraits_pickle = data_folder + 'plantTraits.pkl'
+    if exists(plantTraits_pickle):
+        with open(plantTraits_pickle, 'rb') as plantTraits_fp:
+            plantTraits_bundle = pickle.load(plantTraits_fp)
+    else:
+        plantTraits_bundle = get_rdataset('plantTraits', 'cluster')
+        with open(plantTraits_pickle, 'wb') as plantTraits_fp:
+            pickle.dump(plantTraits_bundle, plantTraits_fp)
+    plantTraits_data = plantTraits_bundle.data
+    logger.info('plantTraits data has variables %s and has %d rows' % (list(plantTraits_data), len(plantTraits_data)))
+    plantTraits_title = plantTraits_bundle.title
+    logger.info('plantTraits data has title %s' % plantTraits_title)
+
+    logger.info('loading plutonium batch data')
+    pluton_pickle = data_folder + 'pluton.pkl'
+    if exists(pluton_pickle):
+        with open(pluton_pickle, 'rb') as pluton_fp:
+            pluton_bundle = pickle.load(pluton_fp)
+    else:
+        pluton_bundle = get_rdataset('pluton', 'cluster')
+        with open(pluton_pickle, 'wb') as pluton_fp:
+            pickle.dump(pluton_bundle, pluton_fp)
+    pluton_data = pluton_bundle.data
+    logger.info('pluton data has variables %s and has %d rows' % (list(pluton_data), len(pluton_data)))
+    pluton_title = pluton_bundle.title
+    logger.info('pluton data has title %s' % pluton_title)
+
     logger.info('loading animal poison data')
     poisons_pickle = data_folder + 'poisons.pkl'
     if exists(poisons_pickle):

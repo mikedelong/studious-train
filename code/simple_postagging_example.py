1e0dc2bccb58967fe66ffc633d4188ae9cc721d0
mikedelong <mikedelong@outlook.com>
2019-02-08 10:32:55 -0500

    feature: use a bigger text and pick out just the nouns, by process of elimination
    Signed-off-by: mikedelong <mikedelong@outlook.com>

--- code/simple_postagging_example.py
+++ code/simple_postagging_example.py
@@ -25,12 +25,27 @@ if __name__ == '__main__':
 
     logger.info('started')
 
-    text = 'European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile ' \
-           'phone market and ordered the company to alter its practices.'
-
+    text = 'General O\'Brien was commissioned in 1981 through the Air Force Reserve Officer Training Corps upon ' \
+           'graduation from the University of New Hampshire. He has served in a wide variety of development, ' \
+           'acquisition and operations roles within the Air Force and the National Reconnaissance Office. His ' \
+           'active-duty career included assignments in requirements development, engineering and program management, ' \
+           'as well as serving as a principal contracting officer for computers, communications, directed energy and ' \
+           'national security space systems, intelligence, surveillance, reconnaissance and policy. His Reserve ' \
+           'leadership responsibilities include network communications, test, maintenance, satellite on-orbit ' \
+           'operations, acquisition and space launch. Prior to his current assignment, General O\'Brien was the ' \
+           'Mobilization Assistant to the Commander, Space and Missiles System Center who also serves as the Air Force ' \
+           'Program Executive Officer for space systems.'
     logger.info(text)
     tagged_text = preprocess(text)
     logger.info(tagged_text)
+
+    parts_tagged = {
+        item[0]: item[1] for item in tagged_text
+    }
+
+    for key, value in parts_tagged.items():
+        if value not in ['VBD', 'VBN', 'IN', 'DT', 'WP', 'VBZ', 'RB', 'JJ', 'TO', 'VBG', 'VBP', 'CC', ',', '.']:
+            logger.info('token {} has tag {}'.format(key, value))
 
     logger.info('done')
 

d4c5c9ea7ceafb9b5b340a6e0bbcd1e2f5eb4dd7
mikedelong <mikedelong@outlook.com>
2019-02-26 07:45:54 -0500

    feature: biomass data from R datasets

--- code/get_datasets.py
+++ code/get_datasets.py
@@ -560,6 +560,21 @@ if __name__ == '__main__':
     bigcity_title = bigcity_bundle.title
     logger.info('bigcity data has title %s' % bigcity_title)
 
+    logger.info('loading biomass data')
+    biomass_pickle = data_folder + 'biomass.pkl'
+    if exists(biomass_pickle):
+        with open(biomass_pickle, 'rb') as biomass_fp:
+            biomass_bundle = pickle.load(biomass_fp)
+    else:
+        biomass_bundle = get_rdataset('biomass', 'DAAG')
+        with open(biomass_pickle, 'wb') as biomass_fp:
+            pickle.dump(biomass_bundle, biomass_fp)
+    biomass_data = biomass_bundle.data
+    logger.info('biomass data has variables %s' % list(biomass_data))
+    logger.info('biomass data has %d rows and %d variables' % biomass_data.shape)
+    biomass_title = biomass_bundle.title
+    logger.info('biomass data has title %s' % biomass_title)
+
     logger.info('loading eating disorder exercise data')
     Blackmore_pickle = data_folder + 'Blackmore.pkl'
     if exists(Blackmore_pickle):
